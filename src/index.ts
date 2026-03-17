fb78353dfdbe910e96fa54759aee2872893f84cb
Alexander Vasilev <46332577+rayonnant@users.noreply.github.com>
2026-03-18 01:32:17 +0300

    feat: add 576 layout

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 import './style.scss';
-console.log('hello world!');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.header__nav-burger');
+    const navList = document.querySelector('.header__nav-list');
+
+    if (burger && navList) {
+        burger.addEventListener('click', () => {
+            navList.classList.toggle('active');
+            burger.classList.toggle('active');
+        });
+    }
+});
