0d258861a763d81a6127798c610b3cc16c0f6e2a
Alexander Vasilev <46332577+rayonnant@users.noreply.github.com>
2026-03-18 21:07:48 +0300

    feat: add stock switch for the second tool

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import './style.scss'
 
+const body: HTMLElement | null = document.body
 const header: HTMLElement | null = document.querySelector('.header')
 const burger: HTMLElement | null = document.querySelector('.header__nav-burger')
 const navList: HTMLElement | null = document.querySelector('.header__nav-list')
-const body: HTMLElement | null = document.body
+const specialStock: HTMLElement | null = document.querySelector('.equipment__stock_special')
 
 const checkScreenSize = (): void => {
     const isDesktop = window.innerWidth >= 1440
@@ -13,14 +14,23 @@ const checkScreenSize = (): void => {
         burger.classList.remove('active')
         header.classList.remove('nav-open')
         body.classList.remove('nav-open')
-        body.style.position = ''
+    }
+
+    if (specialStock) {
+        if (window.innerWidth < 768) {
+            specialStock.textContent = 'В наличии 50 м.'
+            specialStock.classList.remove('equipment__stock_order')
+        } else {
+            specialStock.textContent = 'Под заказ'
+            specialStock.classList.add('equipment__stock_order')
+        }
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     if (burger && navList && header) {
         burger.addEventListener('click', () => {
-            if (window.innerWidth >= 1440) return;
+            if (window.innerWidth >= 1440) return
 
             const isActive: boolean = navList.classList.toggle('active')
             burger.classList.toggle('active')
@@ -30,8 +40,10 @@ document.addEventListener('DOMContentLoaded', () => {
             if (isActive) {
                 window.scrollTo(0, 0)
             }
-        });
+        })
     }
-});
+
+    checkScreenSize()
+})
 
 window.addEventListener('resize', checkScreenSize)
