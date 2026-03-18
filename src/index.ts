a749b55ba78a31332fe9bbf2b726dbd1afe9644a
Alexander Vasilev <46332577+rayonnant@users.noreply.github.com>
2026-03-18 19:39:30 +0300

    feat: add block scroll if menu is active

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,37 @@
-import './style.scss';
+import './style.scss'
+
+const header: HTMLElement | null = document.querySelector('.header')
+const burger: HTMLElement | null = document.querySelector('.header__nav-burger')
+const navList: HTMLElement | null = document.querySelector('.header__nav-list')
+const body: HTMLElement | null = document.body
+
+const checkScreenSize = (): void => {
+    const isDesktop = window.innerWidth >= 1440
+
+    if (isDesktop && navList && burger && header) {
+        navList.classList.remove('active')
+        burger.classList.remove('active')
+        header.classList.remove('nav-open')
+        body.classList.remove('nav-open')
+        body.style.position = ''
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.header__nav-burger');
-    const navList = document.querySelector('.header__nav-list');
-
-    if (burger && navList) {
+    if (burger && navList && header) {
         burger.addEventListener('click', () => {
-            navList.classList.toggle('active');
-            burger.classList.toggle('active');
+            if (window.innerWidth >= 1440) return;
+
+            const isActive: boolean = navList.classList.toggle('active')
+            burger.classList.toggle('active')
+            header.classList.toggle('nav-open')
+            body.classList.toggle('nav-open')
+
+            if (isActive) {
+                window.scrollTo(0, 0)
+            }
         });
     }
 });
+
+window.addEventListener('resize', checkScreenSize)
